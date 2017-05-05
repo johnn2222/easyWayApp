c29756580cba14f7cedcb200d944eee85ad89c70
sunil <sunil.srivastava@gingerwebs.in>
2017-05-05 18:20:34 +0530

    my app

---
+++ www/cordova_plugins.js
@@ -0,0 +1,85 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova.plugins.diagnostic.Diagnostic",
+        "file": "plugins/cordova.plugins.diagnostic/www/android/diagnostic.js",
+        "pluginId": "cordova.plugins.diagnostic",
+        "clobbers": [
+            "cordova.plugins.diagnostic"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.6",
+    "cordova-plugin-device": "1.1.5",
+    "cordova-plugin-inappbrowser": "1.7.0",
+    "cordova-plugin-splashscreen": "4.0.2",
+    "cordova-plugin-statusbar": "2.2.2",
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova.plugins.diagnostic": "3.4.2",
+    "ionic-plugin-keyboard": "2.2.1",
+    "phonegap-plugin-push": "1.10.0",
+    "cordova-plugin-x-socialsharing": "5.1.7"
+};
+// BOTTOM OF METADATA
+});
--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,43 @@
+'use strict';
+var easyWay = angular.module('easyWay', ['ionic','ngRoute','ngSidebarJS','ngCordova']);
 
-var easyWay = angular.module('easyWay', ['ionic','ngRoute','ngSidebarJS']);
-
-    easyWay.run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            // Stuff in here
-        });
-    })
+     
+    
+easyWay.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+});
     
     easyWay.config(function($stateProvider, $urlRouterProvider){
         $stateProvider
             .state('/', {
                 url:'/login',
-                templateUrl: '../src/views/login.html',
+                templateUrl: 'src/views/login.html',
                 controller: 'loginController'
             })             
             .state('/dashboard', {
                 url:'/dashboard',
-                templateUrl: '../src/views/dashBoard.html',
+                templateUrl: 'src/views/dashBoard.html',
                 controller: 'dashBoardController'
             })      
              .state('/notification', {
                 url:'/notification',
-                templateUrl: '../src/views/notification.html',
+                templateUrl: 'src/views/notification.html',
                 controller: 'notificationController'
             })      
         $urlRouterProvider.otherwise('/login');
